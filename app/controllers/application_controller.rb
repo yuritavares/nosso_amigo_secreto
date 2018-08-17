c75efb71fd41c657d71f2af622462df2f7e46c95
Yuri Tavares <y.tavares.info@gmail.com>
2018-08-17 20:53:23 -0300

    adjust controller,  model and factory campaign, and create show and index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
-  def 404
+  def render_404
     redirect_to main_app.root_url
   end
 end
